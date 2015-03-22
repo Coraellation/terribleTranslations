aa6967cbff293f4bb5e3a46ed4b5006722322d93
coralam17@gmail.com <coralam17@gmail.com>
2015-03-21 20:43:00 -0400

    Changes text in paragraphs to pig latin

--- replacePage.js
+++ replacePage.js
@@ -27,8 +27,43 @@ function DOMtoString(document_root) {
 
 function findTags(document_root){
     var newHtml = DOMtoString(document_root);
-    newHtml = newHtml.replace(/(<p>(.*?)<\/p>)/g, "<p>PANDAS</p>");
+    //var arr1 = newHtml.match(/(<p>(.*?)<\/p>)/g);
+    // var arr2 = newHtml.match(/(<ul>(.*?)<\/ul>)/g);
+    //arrUl.concat(newHtml.match(/(<ul>(.*?)<\/ul>)/g));
+    var arr = newHtml.match(/(<p>(.*?)<\/p>)/g);
+
+    //arr.concat(newHtml.match(/(>(.*?)<\/h1>)/g));
+    //console.log(arr[0]);    
+    for (i = 0; i < arr.length; i++){
+        origSentence = arr[i];
+        console.log(arr[i]);
+        if (arr[i].substr(0,4) == "<ul>"){
+            openTag = arr[i].substr(0,4); //for ul tags
+            closeTag = arr[i].substr(arr[i].length-6, 5);
+            arr[i] = arr[i].substr(4,arr[i].length-9);
+
+        }else{
+            openTag = arr[i].substr(0,3); //for p tags
+            closeTag = arr[i].substr(arr[i].length-5, 4);
+            arr[i] = arr[i].substr(3,arr[i].length-7);
+        }
+        
+
+        words = arr[i].split(" ");
+        for (j = 0; j < words.length;j++){
+            words[j]= words[j].substr(1,words[j].length-1)+words[j].charAt(0)+'ay';
+            //console.log(words[j]);
+        }
+        
+        newSentence = words.join(" ");
+        //console.log(newSentence);
+        newSentence = openTag + newSentence + closeTag;
+        newHtml = newHtml.replace(origSentence, newSentence);
+
+    }
+//    newHtml = newHtml.replace(/(<p>(.*?)<\/p>)/g, "<p>OCTOPUS</p>");
     return newHtml;
 }
 
 document.write(findTags(document));
+document.close();
